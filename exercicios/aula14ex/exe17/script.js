b2d28f5fa456fb9c0117a29cdeaff1aeb46aa79a
Luiz Felipe <92935814+LuizFesf@users.noreply.github.com>
2022-02-22 21:38:22 -0300

    js

--- exercicios/aula14ex/exe17/script.js
+++ exercicios/aula14ex/exe17/script.js
@@ -1,1 +1,25 @@
-alert("Olá!")
+function gerar()
+{
+    var num = document.getElementById("txtn1")
+    var tab = document.getElementById("seltab")
+
+    if(num.value.length == 0)
+    {
+        alert("Por favor, digite um número!")    
+    }
+    else
+    {  
+        var num = Number(num.value)
+        var c = 1
+        tab.innerHTML = ''
+        while(c <= 10)
+        {
+            
+            var item = document.createElement("option")
+            item.text = `${num} x ${c} = ${num*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
